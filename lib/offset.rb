182d403fc5971e0fbd61173f6cee1d928ff718df
maxscores <maxstackhouse@gmail.com>
2017-10-16 13:20:22 -0600

    Add date_code to Offset, Remove from Key_gen

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,27 +2,38 @@ require './lib/key_generator'
 require 'date'
 
 class Offset
-  attr_reader :key, :date_code, :date
+  attr_reader :key, :date
 
   def initialize(key, date)
     @key = key
     @date = date
-    if date.class == Date
-      @date_code = KeyGenerator.new.date_code(date)
-    elsif date.class == String
-      @date_code = date.to_i
-    end
+    # if date.class == Date
+    #   @date_code = date_code(date)
+    # elsif date.class == String
+    #   @date_code = date.to_i
+    # end
     #put something to allow date_code instead
   end
 
-  def date_offset
-    date_offset  = []
-    squared = date_code**2
-    4.times do |time|
-      date_offset << squared.digits[3 - time]
-      # check for enumerables here for refactoring^
+  def date_code(date)
+    if date.class == Date
+      day, month, year = date.day, date.mon, date.year%100
+      month = "%02d" % month.to_s
+      year = "%02d" % year.to_s
+      "#{day}#{month}#{year}".to_i
+    elsif date.class == String
+      date.to_i
     end
-    date_offset = squared.digits.reverse[0..3].map {|offset| offset}
+  end
+
+  def date_offset
+    # date_offset  = []
+    squared = date_code(date)**2
+    # 4.times do |time|
+    #   date_offset << squared.digits[3 - time]
+    #   # check for enumerables here for refactoring^
+    # end
+    squared.digits.reverse[-4..-1].map {|offset| offset}
   end
 
   def key_offset
@@ -31,6 +42,13 @@ class Offset
       key_offset << [key[time],key[time + 1]].join("").to_i
     end
     key_offset
+    # counter = (-1)
+    # keys = key.split("")
+    # keys.map do |key|
+    #   counter += 1
+    #   break if counter==4
+    #   [keys[counter], keys[counter+1]].join("").to_i
+    # end
   end
 
   def offset

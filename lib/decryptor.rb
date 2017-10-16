9d083f5d9a1a9bce1d53994ddd0266f92ec1d5dd
heybluguy <heybluguy@yahoo.com>
2017-10-16 14:54:45 -0600

    breakout classes

---
+++ lib/decryptor.rb
@@ -0,0 +1,33 @@
+require './lib/offset'
+
+class Decryptor
+
+  def decrypt(message, key, date = default_date)
+    offset = Offset.new(key, date).offset
+    character_values = format_message(message)
+    decrypted_character_values = decrypt_characters(character_values, offset)
+    decrypted_character_values.join("")
+  end
+
+  def format_message(message)
+    message.split("").map {|character| characters[character]}
+  end
+
+  def decrypt_characters(character_values, offset)
+    counter = (-1)
+    character_values.map do |value|
+      counter += 1
+      new_value = (value - offset[counter%4])% characters.count
+      characters.key(new_value)
+    end
+  end
+
+  def decrypt_file(file_to_read, key, date_code)
+    file = File.readlines(file_to_read)
+    decrypted_file = file.map do |line|
+      decrypt(line.chomp, key, date_code)
+    end
+    decrypted_file.join("")
+  end
+
+end
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,6 @@
 require './lib/offset'
 require './lib/characters'
+require './lib/encryptor'
 require 'date'
 require 'pry'
 
@@ -14,58 +15,47 @@ class Enigma
   end
 
   def encrypt(message, key = random_key, date = default_date)
-    offset = Offset.new(key, date).offset
-    character_values = format_message(message)
-    encrypted_character_values = encrypt_characters(character_values, offset)
-    encrypted_character_values.join("")
-  end
+    encrypt = Encryptor.new.encrypt(message, key, date)
 
-  def format_message(message)
-    message.split("").map do |character|
-      characters[character]
-    end
-  end
 
-  def encrypt_characters(character_values, offset)
-    counter = (-1)
-    character_values.map do |value|
-      counter += 1
-      new_value = (value + offset[counter%4])% characters.count
-      characters.key(new_value)
-    end
   end
+  # def encrypt(message, key = random_key, date = default_date)
+  #   offset = Offset.new(key, date).offset
+  #   character_values = format_message(message)
+  #   encrypted_character_values = encrypt_characters(character_values, offset)
+  #   encrypted_character_values.join("")
+  # end
+  #
+  # def format_message(message)
+  #   message.split("").map {|character| characters[character]}
+  # end
+  #
+  # def encrypt_characters(character_values, offset)
+  #   counter = (-1)
+  #   character_values.map do |value|
+  #     counter += 1
+  #     new_value = (value + offset[counter%4])% characters.count
+  #     characters.key(new_value)
+  #   end
+  # end
 
-  def decrypt(message, key, date = default_date)
-    offset = Offset.new(key, date).offset
-    character_values = format_message(message)
-    decrypted_character_values = decrypt_characters(character_values, offset)
-    decrypted_character_values.join("")
-  end
 
-  def decrypt_characters(character_values, offset)
-    counter = (-1)
-    character_values.map do |value|
-      counter += 1
-      new_value = (value - offset[counter%4])% characters.count
-      characters.key(new_value)
-    end
-  end
 
-  def encrypt_file(file_to_read)
-    file = File.readlines(file_to_read)
-    encrypted_file = file.map do |line|
-      encrypt(line.chomp)
-    end
-    encrypted_file.join("")
-  end
+  # def encrypt_file(file_to_read)
+  #   file = File.readlines(file_to_read)
+  #   encrypted_file = file.map do |line|
+  #     encrypt(line.chomp)
+  #   end
+  #   encrypted_file.join("")
+  # end
 
-  def decrypt_file(file_to_read, key, date_code)
-    file = File.readlines(file_to_read)
-    decrypted_file = file.map do |line|
-      decrypt(line.chomp, key, date_code)
-    end
-    decrypted_file.join("")
-  end
+  # def decrypt_file(file_to_read, key, date_code)
+  #   file = File.readlines(file_to_read)
+  #   decrypted_file = file.map do |line|
+  #     decrypt(line.chomp, key, date_code)
+  #   end
+  #   decrypted_file.join("")
+  # end
 
   def write_file(file_to_write, text_to_write)
     file = File.new(file_to_write, 'w')

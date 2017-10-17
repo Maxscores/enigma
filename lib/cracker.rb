7c7c6db28666711702d6afed2dcd6e9d16639f0f
maxscores <maxstackhouse@gmail.com>
2017-10-16 19:18:47 -0600

    Refactor cracker and test

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -9,20 +9,32 @@ class Cracker
     @characters = Characters.new.characters
   end
 
-  def crack(encryption, date = Date.today)
-    date_offset = Offset.new().date_offset
-    crack_offset = encryption.length%4
-    match_characters = encryption[(-1)*(crack_offset + 4)..(-1)*(crack_offset + 1)]
-    known_characters = [".",".","e","n","d",".","."][(-1)*(crack_offset+4)..(-1)*(crack_offset+1)]
-    match_values = match_characters.split("").map {|character| characters[character]}
-    known_values = known_characters.map {|character| characters[character]}
+  def crack(encryption, date = Date.today, key = nil)
+    date_offset = Offset.new(key, date).date_offset
+    crack_offset = (encryption.length%4) * (-1)
+    match_values = encrypted_end_values(encryption, crack_offset)
+    known_values = known_end_values(crack_offset)
     key_breaker = match_values.zip(known_values, date_offset)
+    key = key_finder(key_breaker)
+  end
+
+  def encrypted_end_values(encryption, crack_offset)
+    encrypted_match = encryption[(crack_offset - 4)..(crack_offset - 1)]
+    encrypted_match.split("").map {|character| characters[character].to_i}
+  end
+
+  def known_end_values(crack_offset)
+    known_end = [".",".","e","n","d",".","."]
+    matched_end = known_end[(crack_offset-4)..(crack_offset-1)]
+    matched_end.map {|character| characters[character].to_i}
+  end
+
+  def key_finder(key_breaker)
     key_offset = key_breaker.map do |matched|
       result = (matched[0] - matched[1] - matched[2])%39
     end
     key = key_offset[0].to_s + key_offset[2].to_s + (key_offset[3]%10).to_s
   end
-
 
 
 end
--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -10,11 +10,20 @@ class CrackerTest < Minitest::Test
     assert_instance_of Cracker, cracker
   end
 
+  def test_encrypted_end_values
+    cracker = Cracker.new
+    encryption = "cl8sjefvmsze7f1u.fu"
+    crack_offset = (-3)
+    result = cracker.encrypted_end_values(encryption, crack_offset)
+
+    assert_equal [33, 6, 27, 21], result
+  end
+
   def test_crack
     cracker = Cracker.new
     encryption = "cl8sjefvmsze7f1u.fu"
 
-    assert_equal '30147', cracker.crack(encryption)
+    assert_equal '30147', cracker.crack(encryption, '161017')
   end
 
 
